da03cace976c994885edf1a9eab53d6cf350bfb1
ddominik16 <dominik.karimovic123@gmail.com>
2025-01-14 17:46:34 +0100

    Update zad1.py

--- zadaca_3/zad1.py
+++ zadaca_3/zad1.py
@@ -1,32 +1,31 @@
-class Parser:
-    def _parseLine(self, line, n):
-        l = line.split("//")[0].split()
-        if len(l) == 0 or len(l[0]) == 0:
-            return ""
-    
-        if l[0] == "push":
-            if len(l) == 3:
-                return "//" + " ".join(l) + "\n" + self._push(l[1], l[2], n)
-            else:
-                self._flag = False
-                Parser._error("Parser", n, "Undefined command");
-                return ""
-    
-        elif l[0] == "pop":
-            if len(l) == 3:
-                return "//" + " ".join(l) + "\n" + self._pop(l[1], l[2], n)
-            else:
-                self._flag = False
-                Parser._error("Parser", n, "Undefined command");
-                return ""
+def _parse_lines(self):
+    self._comment = False    
+    self._iter_lines(self._parse_line)
+
+def _parse_line(self, line, p, o):
+    l = ""
+    i = 0
+    while i < len(line) - 1:
+        p = line[i] + line[i + 1]
+
+        if (self._comment == False and p == "/*") or (self._comment and p == "*/"):
+            self._comment = not self._comment
+            i += 1
+        elif self._comment == False and p == "*/":
+            self._flag = False
+            self._line = o
+            self._errm = "zatvalranje neotvorenih komentara"
+        elif (p == "//"):
+            break
+        elif line[i].isspace() == False and self._comment == False:
+            l += line[i]
         # ZADATAK 1
         elif line[0] == "@" and line[1] in "0123456789": 
             if int(line[1:]) > 32768 or int(line[1:]) < 0:
                 self._flag = False
-                Parser._error("Parser", n, "Invalid memory location.")
+                self._line = o
+                self._errm = "Nispravna memorijska adresa."
                 return ""
-            
-        elif len(l) == 1:
-            return "//" + " ".join(l) + "\n" + self._comm(l[0], n)
-    
-        return ""
+        i += 1
+
+    return l
